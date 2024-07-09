bd82596d7d1a9159994bf8d8ad1a3e2efa9f6eea
robert-allen <allen.robert79@gmail.com>
2024-07-09 14:32:46 +0100

    good place to save

--- src/components/api.js
+++ src/components/api.js
@@ -5,19 +5,21 @@ const clevelandApi = axios.create({
 });
 
 const chicagoApi = axios.create({
-  baseURL: "https://api.harvardartmuseums.org",
+  baseURL: "https://www.rijksmuseum.nl/api/nl",
 });
 
 // &limit=${itemLimit}&skip=${skip}
 // GET ALL ClArtworks
 const getAllClArtworks = (page, itemLimit, type, department) => {
   const skip = itemLimit * page;
-  let typeStr = ""
-  let departmentStr = ""
-  if (department) departmentStr = `&department=${department}`
-    if (type) typeStr = `&type=${type}`
+  let typeStr = "";
+  let departmentStr = "";
+  if (department) departmentStr = `&department=${department}`;
+  if (type) typeStr = `&type=${type}`;
   return clevelandApi
-    .get(`/artworks?has_image=1${typeStr}${departmentStr}&page=2&limit=${itemLimit}&skip=${skip}`)
+    .get(
+      `/artworks?has_image=1${typeStr}${departmentStr}&page=2&limit=${itemLimit}&skip=${skip}`
+    )
     .then((response) => {
       // console.log(response.data.data)
       return [response.data.data, response.data.info.total];
@@ -31,28 +33,66 @@ const getSingleClArtwork = (clartwork_id) => {
   });
 };
 
-// GET ALL ChicagoArtworks
-const getAllChicagoArtworks = (page, itemLimit, type, department) => {
-  const skip = itemLimit * page;
-  let typeStr = ""
-  let departmentStr = ""
+const getAllChicagoArtworks = ( pageNo, itemLimit, selectedType, selectedDepartment, sortCriteria, sortOrder ) => {
+  
+  let sort=""
+  if(sortCriteria === "relevance")
+    {sort = "&s=relevance"}
+  else if (sortCriteria === "objecttype") {
+    sort = "&s=objecttype"
+  } else if (sortCriteria === "chronologic") {
+    if (sortOrder === "ascending") {
+      sort = "&s=chronologic"
+    } else { sort = "&s=achronologic"}
+  } else if (sortCriteria === "artist") {
+    if (sortOrder === "ascending") {
+      sort = "&s=artist"
+    } else { sort = "&s=artistdesc"}
+  }
 
   return chicagoApi
-    .get(`/object?hasimage=true&size=30&page=1&apikey=placeholder_key`)
+    .get(`/collection?key=25T7NCOQ&imgonly=true&ps=${itemLimit}&p=${pageNo + 1}&culture=en${sort}`)
     .then((response) => {
-      console.log(response.data)
-      return [response.data.records, response.data.info.totalrecords];
+      console.log (response)
+      const artObjects = response.data.artObjects;
+      const promises = artObjects.map((artwork) => {
+        return getSingleChicagoArtwork(artwork.objectNumber);
+      });
+
+      return Promise.all(promises)
+        .then((singleArtworks) => {
+          return [singleArtworks, response.data.count];
+        })
+        .catch((error) => {
+          console.error("Error fetching single artworks:", error);
+          throw error;
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching Chicago artworks:", error);
+      throw error;
     });
 };
 
 // GET SINGLE ChicagoArtwork
 const getSingleChicagoArtwork = (chicagoartwork_id) => {
-  console.log(chicagoartwork_id);
-  console.log(`/artworks/${chicagoartwork_id}`);
-  return chicagoApi.get(`/artworks/${chicagoartwork_id}`).then((response) => {
-    console.log("single artwork response, Chicago", response.data.data);
-    return response.data.data;
-  });
+  return chicagoApi
+    .get(`/collection/${chicagoartwork_id}?key=25T7NCOQ&culture=en`)
+    .then((response) => {
+      return response.data.artObject;
+    })
+    .catch((error) => {
+      console.error(
+        `Error fetching Chicago artwork ${chicagoartwork_id}:`,
+        error
+      );
+      throw error;
+    });
 };
 
-export { getAllClArtworks, getSingleClArtwork, getAllChicagoArtworks, getSingleChicagoArtwork };
+export {
+  getAllClArtworks,
+  getSingleClArtwork,
+  getAllChicagoArtworks,
+  getSingleChicagoArtwork,
+};
