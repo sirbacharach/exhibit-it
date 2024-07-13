0bf1299ea86e28a8c16a285e1f79ea98844818c1
robert-allen <allen.robert79@gmail.com>
2024-07-13 16:23:57 +0100

    completes first museum, but still needs names changing appropriately and links to the museum

--- src/components/api.js
+++ src/components/api.js
@@ -23,13 +23,12 @@ const getAllClArtworks = async (
   type,
   datingPeriod,
   technique
-  ) => {
-
+) => {
   const skip = itemLimit * pageNo;
   let searchStr = "";
   let materialStr = "";
-  let placeStr = ""
-  let personStr = ""
+  let placeStr = "";
+  let personStr = "";
   let orderBy = "";
   let orderSort = "";
   if (sortCriteria) {
@@ -38,15 +37,13 @@ const getAllClArtworks = async (
   }
   if (sortOrder) orderSort = `&order_sort=${sortOrder}`;
 
-if (selectedMaterial) materialStr = `&q_material_technique=${selectedMaterial}`
-if (selectedPlace) placeStr = `&q_place_name=${selectedPlace}`
-if (selectedPerson) personStr = `&q_place_name=${selectedPerson}`
-if (userSearch) searchStr = `&kw_object_type=${userSearch}`
+  if (selectedMaterial)
+    materialStr = `&q_material_technique=${selectedMaterial}`;
+  if (selectedPlace) placeStr = `&q_place_name=${selectedPlace}`;
+  if (selectedPerson) personStr = `&q_place_name=${selectedPerson}`;
+  if (userSearch) searchStr = `&kw_object_type=${userSearch}`;
 
-  try {
-    const response = await clevelandApi.get(
-      `/objects/search?page_size=10&images=1&page_size=${itemLimit}&page=${pageNo +1}${orderBy}${orderSort}${materialStr}${placeStr}${personStr}${searchStr}`
-    );
+  try { const response = await clevelandApi.get(`/objects/search?page_size=10&images=1&page_size=${itemLimit}&page=${pageNo + 1}${orderBy}${orderSort}${materialStr}${placeStr}${personStr}${searchStr}`);
     return [response.data.records, response.data.info.record_count];
   } catch (error) {
     console.error("Error fetching Cleveland artworks:", error);
@@ -78,6 +75,7 @@ const getAllChicagoArtworks = async (
   material,
   technique
 ) => {
+
   let sort = "";
   if (sortCriteria === "relevance") {
     sort = "&s=relevance";
@@ -97,25 +95,16 @@ const getAllChicagoArtworks = async (
     }
   }
 
-  let makerString = principalMaker
-    ? `&involvedMaker=${principalMaker.split(" ").join("+")}`
-    : "";
+  let makerString = principalMaker ? `&involvedMaker=${principalMaker.split(" ").join("+")}` : "";
   let typeString = type ? `&type=${type.split(" ").join("+")}` : "";
   let periodString = datingPeriod ? `&f.dating.period=${datingPeriod}` : "";
   let placeString = place ? `&place=${place.split(" ").join("+")}` : "";
-  let materialString = material
-    ? `&material=${material.split(" ").join("+")}`
-    : "";
-  let techniqueString = technique
-    ? `&technique=${technique.split(" ").join("+")}`
-    : "";
+  let materialString = material ? `&material=${material.split(" ").join("+")}` : "";
+  let techniqueString = technique ? `&technique=${technique.split(" ").join("+")}` : "";
 
   try {
     const response = await chicagoApi.get(
-      `/collection?key=25T7NCOQ&imgonly=true&ps=${itemLimit}&p=${
-        pageNo + 1
-      }&culture=en${sort}${makerString}${typeString}${periodString}${placeString}${materialString}${techniqueString}`
-    );
+      `/collection?key=25T7NCOQ&imgonly=true&ps=${itemLimit}&p=${pageNo + 1}&culture=en${sort}${makerString}${typeString}${periodString}${placeString}${materialString}${techniqueString}`);
     const artObjects = response.data.artObjects;
     const promises = artObjects.map((artwork) =>
       getSingleChicagoArtwork(artwork.objectNumber)
@@ -123,10 +112,8 @@ const getAllChicagoArtworks = async (
 
     const singleArtworks = await Promise.all(promises);
     return [singleArtworks, response.data.count];
-  } catch (error) {
-    console.error("Error fetching Chicago artworks:", error);
-    throw error;
-  }
+  } catch (error){
+    console.error("Error fetching Chicago artworks:", error); throw error; }
 };
 
 // GET SINGLE ChicagoArtwork
@@ -136,11 +123,7 @@ const getSingleChicagoArtwork = async (chicagoartwork_id) => {
       `/collection/${chicagoartwork_id}?key=25T7NCOQ`
     );
     return response.data.artObject;
-  } catch (error) {
-    console.error(
-      `Error fetching Chicago artwork ${chicagoartwork_id}:`,
-      error
-    );
+  } catch (error) { console.error(`Error fetching Chicago artwork ${chicagoartwork_id}:`, error);
     throw error;
   }
 };
